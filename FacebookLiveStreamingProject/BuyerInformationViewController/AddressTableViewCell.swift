ccba7f724eee1b4c3710be1655c97c684dda54f0
Albert <chenyihao@Alberts-MacBook-Pro.local>
2019-01-24 15:31:13 +0800

    add README.md

---
+++ FacebookLiveStreamingProject/BuyerInformationViewController/AddressTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddressTableViewCell.swift
+//  FacebookLiveStreamingProject
+//
+//  Created by Albert on 2019/1/24.
+//  Copyright © 2019 Albert.C. All rights reserved.
+//
+
+import UIKit
+
+class AddressTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FacebookLiveStreamingProject/BuyerInformationViewController/BuyerInformationViewControllerExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  BuyerInformationViewControllerExtensions.swift
+//  FacebookLiveStreamingProject
+//
+//  Created by Albert on 2019/1/24.
+//  Copyright © 2019 Albert.C. All rights reserved.
+//
+
+import Foundation
